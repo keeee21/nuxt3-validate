33b089be119595ed4918b049642a1a9249317eb8
keeee21 <keisuke.u211016@gmail.com>
2024-12-08 22:09:30 +0900

    feat: yup

--- pages/yup/index.vue
+++ pages/yup/index.vue
@@ -1,11 +1,41 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { string as yupString, object as yupObject, bool as yupBool } from 'yup';
 
-// フォームデータ
-const formData = ref({
+const validationErrors = ref<string[]>([]);
+
+const sampleData = ref({
   textField: '',
   radioOption: '',
   dropdown: '',
+  checkbox: false,
+});
+
+const schema = yupObject().shape({
+  textField: yupString().required('テキスト入力は必須です').max(5, '5文字以内で入力してください'),
+  radioOption: yupString().required('ラジオボタンは必須です'),
+  dropdown: yupString().required('ドロップダウンは必須です'),
+  checkbox: yupBool().oneOf([true], 'チェックボックスは必須です'),
+});
+
+const saveAction = async () => {
+  console.log(sampleData.value);
+  try {
+    //  { abortEarly: false } にすると、エラーが複数ある場合に全て表示する
+    await schema.validate(sampleData.value, { abortEarly: false });
+    window.alert('バリデーション成功');
+  } catch (error) {
+    validationErrors.value = error.errors;
+    window.alert(error);
+  }
+
+}
+
+// フォームデータ
+const formData = ref({
+  textField: null,
+  radioOption: null,
+  dropdown: null,
   checkbox: false,
 });
 
@@ -25,14 +55,22 @@ const selectOptions = ref([
 <template>
   <h1>一般的なフォーム</h1>
 
+  <div v-if="validationErrors.length > 0">
+    <ul>
+      <li v-for="error in validationErrors" :key="error">
+        {{ error }}
+      </li>
+    </ul>
+  </div>
+
   <!-- テキスト入力 -->
   <!-- 文字数カウントする -->
   <div>
     <label for="textField">テキスト入力:</label>
     <input 
       type="text" 
-      id="textField" 
-      v-model="formData.textField" 
+      id="textField"
+      v-model="sampleData.textField" 
     />
   </div>
   <br>
@@ -49,7 +87,7 @@ const selectOptions = ref([
         :id="radioOption.label" 
         type="radio" 
         :value="radioOption.value" 
-        v-model="formData.radioOption"
+        v-model="sampleData.radioOption"
       />
       {{ radioOption.label }}
     </label>
@@ -61,7 +99,7 @@ const selectOptions = ref([
     <label for="dropdown">ドロップダウン:</label>
     <select 
       id="dropdown" 
-      v-model="formData.dropdown"
+      v-model="sampleData.dropdown"
     >
       <option disabled value="">選択してください</option>
       <option 
@@ -80,7 +118,7 @@ const selectOptions = ref([
     <label>
       <input 
         type="checkbox" 
-        v-model="formData.checkbox" 
+        v-model="sampleData.checkbox" 
       />
       チェックを入れてください
     </label>
@@ -88,5 +126,5 @@ const selectOptions = ref([
   <br>
 
   <!-- 送信ボタン -->
-  <button type="submit">送信</button>
+  <button type="button" @click="saveAction">送信</button>
 </template>
